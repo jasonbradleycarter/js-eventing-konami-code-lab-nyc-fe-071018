aad2bcbd552dc27a5072736ad6c3f906a3ab0baf
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-22 00:16:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+// we have an array of key codes (event.which)
+// add an event listener somewhere so that when we press this, something happens
+// we need to keep track of where we are in the combo
+
+
 function init() {
   // Write your JavaScript code inside the init() function
 
