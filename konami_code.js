749a0848c763f80ef0197d2931de5041b55dfa94
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-22 00:24:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,8 +4,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 // add an event listener somewhere so that when we press this, something happens
 // we need to keep track of where we are in the combo
 
-
 function init() {
+  // tracker keeps track of where we are in the array
+  const tracker = 0;
   // Write your JavaScript code inside the init() function
-
+  const body - document.querySelector('body');
+  body.addEventListener('keydown', (event) => {
+    if (event.which === code[tracker]) {
+      console.log('Success!')
+      tracker++
+    } else {
+      console.log('youve messed up :(');
+      tracker = 0
+    }
+  })
 }
