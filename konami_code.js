79198708098832afce1b61ed93a021c21f9eda37
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-22 00:27:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,4 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 // we have an array of key codes (event.which)
 // add an event listener somewhere so that when we press this, something happens
@@ -8,16 +8,16 @@ function init() {
   // tracker keeps track of where we are in the array
   const tracker = 0;
   // Write your JavaScript code inside the init() function
-  const body - document.querySelector('body');
+  const body = document.querySelector('body');
   body.addEventListener('keydown', (event) => {
     if (event.which === code[tracker]) {
-      console.log('Success!')
-      tracker++
+      console.log('Success!');
+      tracker++;
     } else {
       console.log('youve messed up :(');
-      tracker = 0
+      tracker = 0;
     }
-  })
+  });
 }
 
-init()
+init();
