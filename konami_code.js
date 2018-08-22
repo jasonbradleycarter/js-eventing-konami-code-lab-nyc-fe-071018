1c60dc97200ba01aa6d826a7bbb698dafbeaad48
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-22 00:48:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,6 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function onKeyDownHandler(event) {
   if (tracker === code.length) {
         event.preventDefault()
+        return
       }
     
     if (event.which === code[tracker]) {
@@ -28,8 +29,7 @@ function init() {
   const body = document.querySelector('body');
   body.addEventListener('keydown', (event) => 
     
-      
-  })
+  )
 }
 
 init();
